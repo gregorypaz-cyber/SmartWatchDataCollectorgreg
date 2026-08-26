92407b207ba73274222b9e57e235b4a8fe1f75c3
gregorypaz-cyber <gregorypaz@gmail.com>
2026-08-26 14:27:09 +0000

    novo18

--- App/utils/config/constants.js
+++ App/utils/config/constants.js
@@ -1,5 +1,8 @@
 export const ENDPOINT = "https://anmyzcmrrgjvqorjqrvr.supabase.co/rest/v1/terra_wearable_data"
 export const KEY = "sb_publishable_nIuf4ql2djTbhumb21oGLA_ekc0xX_G"
-export const STORAGES = ["basic", "sleep", "heart", "spo2", "stress", "wear"]
+
+// Atualizado com as categorias reais que você quer coletar
+export const STORAGES = ["vape", "drink", "food", "alcohol", "pee", "poo", "headache"]
+
 export const CHUNK_SIZE = 250
 export const ROW_LENGTH = 100
