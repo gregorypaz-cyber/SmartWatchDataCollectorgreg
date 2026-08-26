b64398faab5a1a446273e1c08df4640fb5f68940
gregorypaz-cyber <gregorypaz@gmail.com>
2026-08-26 13:22:43 +0000

    novo10

--- App/pages/index.js
+++ App/pages/index.js
@@ -9,336 +9,129 @@ Page({
   build() {
     console.log("App build begin")
 
-    text_widget = hmUI.createWidget(hmUI.widget.TEXT, {
+    // Tela de loading inicial
+    hmUI.createWidget(hmUI.widget.TEXT, {
       x: 0,
       y: 0,
-      w: 480,
-      h: 480,
+      w: 390,
+      h: 450,
       color: 0xffffff,
-      text_size: 80,
+      text_size: 40,
       align_h: hmUI.align.CENTER_H,
       align_v: hmUI.align.CENTER_V,
-      text_style: hmUI.text_style.NONE,
-      text: "LOADING...",
+      text: "DATA COLLECTOR\nPronto para uso!",
     });
 
-    // Send data to server
-    fetchAndSendChunks(storages, messageBuilder, text_widget);
+    // Altura útil ajustada para a tela do Bip 6 (450px por página)
+    const pageH = 450;
 
-
-    hmUI.setScrollView(true, px(480), 7, true)
+    // Ativa o scroll vertical para passar pelas 7 páginas de categorias
+    hmUI.setScrollView(true, px(pageH), 7, true)
     
-    // PAGE #1  
+    // PAGE #1: VAPE
     hmUI.createWidget(hmUI.widget.FILL_RECT, {
-      x: 0,
-      y: px(480) * 1,
-      w: px(480),
-      h: px(480),
-      color: 0x003049
+      x: 0, y: pageH * 1, w: 390, h: pageH, color: 0x003049
     })
-
     hmUI.createWidget(hmUI.widget.BUTTON, {
-      x: px(140),      
-      y: px(140) + px(480) * 1,
-      w: 200,
-      h: 200,
-      normal_src: 'vape.png',
-      press_src: 'vape_press.png',
+      x: 95, y: 125 + pageH * 1, w: 200, h: 200,
+      normal_src: 'vape.png', press_src: 'vape_press.png',
       click_func: () => {
-          console.log("VAPE BUTTON")
-        
-          storages.vape.append(time.utc)
-          console.log(storages.vape.get(0, Infinity))
-
-          vibrate.stop()
-          vibrate.scene = 25
-          vibrate.start()      
+        storages.vape.append(time.utc)
+        vibrate.stop(); vibrate.scene = 25; vibrate.start();
       }
     })
 
-    // PAGE #2 
+    // PAGE #2: DRINKS
     hmUI.createWidget(hmUI.widget.FILL_RECT, {
-      x: 0,
-      y: px(480) * 2,
-      w: px(480),
-      h: px(480),
-      color: 0xd62828
+      x: 0, y: pageH * 2, w: 390, h: pageH, color: 0xd62828
     })
-
     hmUI.createWidget(hmUI.widget.BUTTON, {
-      x: px(85),      
-      y: px(85) + px(480) * 2,
-      w: 150,
-      h: 150,
-      normal_src: 'drink_1.png',
-      press_src: 'drink_1_press.png',
-      click_func: () => {
-        console.log("Drink 1 BUTTON")
-
-        storages.drink.append(`${time.utc};${1}`)
-  
-        vibrate.stop()
-        vibrate.scene = 25
-        vibrate.start()      
-      }
+      x: 40, y: 80 + pageH * 2, w: 140, h: 140,
+      normal_src: 'drink_1.png', press_src: 'drink_1_press.png',
+      click_func: () => { storages.drink.append(`${time.utc};${1}`); vibrate.stop(); vibrate.scene = 25; vibrate.start(); }
     })
-
     hmUI.createWidget(hmUI.widget.BUTTON, {
-      x: px(250),      
-      y: px(85) + px(480) * 2,
-      w: 150,
-      h: 150,
-      normal_src: 'drink_2.png',
-      press_src: 'drink_2_press.png',
-      click_func: () => {
-        console.log("Drink 2 BUTTON")
-
-        storages.drink.append(`${time.utc};${3}`)     
-
-        vibrate.stop()
-        vibrate.scene = 25
-        vibrate.start()         
-      }
+      x: 210, y: 80 + pageH * 2, w: 140, h: 140,
+      normal_src: 'drink_2.png', press_src: 'drink_2_press.png',
+      click_func: () => { storages.drink.append(`${time.utc};${3}`); vibrate.stop(); vibrate.scene = 25; vibrate.start(); }
     })
-
     hmUI.createWidget(hmUI.widget.BUTTON, {
-      x: px(85),      
-      y: px(250) + px(480) * 2,
-      w: 150,
-      h: 150,
-      normal_src: 'drink_3.png',
-      press_src: 'drink_3_press.png',
-      click_func: () => {
-        console.log("Drink 3 BUTTON")
-
-        storages.drink.append(`${time.utc};${5}`)    
-
-        vibrate.stop()
-        vibrate.scene = 25
-        vibrate.start()          
-      }
+      x: 40, y: 235 + pageH * 2, w: 140, h: 140,
+      normal_src: 'drink_3.png', press_src: 'drink_3_press.png',
+      click_func: () => { storages.drink.append(`${time.utc};${5}`); vibrate.stop(); vibrate.scene = 25; vibrate.start(); }
     })
-
     hmUI.createWidget(hmUI.widget.BUTTON, {
-      x: px(250),      
-      y: px(250) + px(480) * 2,
-      w: 150,
-      h: 150,
-      normal_src: 'drink_4.png',
-      press_src: 'drink_4_press.png',
-      click_func: () => {
-        console.log("Drink 4 BUTTON")
-
-        storages.drink.append(`${time.utc};${20}`)
-
-        vibrate.stop()
-        vibrate.scene = 25
-        vibrate.start()
-      }
+      x: 210, y: 235 + pageH * 2, w: 140, h: 140,
+      normal_src: 'drink_4.png', press_src: 'drink_4_press.png',
+      click_func: () => { storages.drink.append(`${time.utc};${20}`); vibrate.stop(); vibrate.scene = 25; vibrate.start(); }
     })
 
-
-    // PAGE #3
+    // PAGE #3: FOOD
     hmUI.createWidget(hmUI.widget.FILL_RECT, {
-      x: 0,
-      y: px(480) * 3,
-      w: px(480),
-      h: px(480),
-      color: 0xf77f00
+      x: 0, y: pageH * 3, w: 390, h: pageH, color: 0xf77f00
     })
-
     hmUI.createWidget(hmUI.widget.BUTTON, {
-      x: px(85),      
-      y: px(85) + px(480) * 3,
-      w: 150,
-      h: 150,
-      normal_src: 'food_1.png',
-      press_src: 'food_1_press.png',
-      click_func: () => {
-        console.log("food 1 BUTTON")
-    
-        storages.food.append(`${time.utc};${1}`)
-
-        vibrate.stop()
-        vibrate.scene = 25
-        vibrate.start() 
-      }
+      x: 40, y: 80 + pageH * 3, w: 140, h: 140,
+      normal_src: 'food_1.png', press_src: 'food_1_press.png',
+      click_func: () => { storages.food.append(`${time.utc};${1}`); vibrate.stop(); vibrate.scene = 25; vibrate.start(); }
     })
-
     hmUI.createWidget(hmUI.widget.BUTTON, {
-      x: px(250),      
-      y: px(85) + px(480) * 3,
-      w: 150,
-      h: 150,
-      normal_src: 'food_2.png',
-      press_src: 'food_2_press.png',
-      click_func: () => {
-        console.log("food 2 BUTTON")
-
-        storages.food.append(`${time.utc};${2}`)
-
-        vibrate.stop()
-        vibrate.scene = 25
-        vibrate.start()
-      }
+      x: 210, y: 80 + pageH * 3, w: 140, h: 140,
+      normal_src: 'food_2.png', press_src: 'food_2_press.png',
+      click_func: () => { storages.food.append(`${time.utc};${2}`); vibrate.stop(); vibrate.scene = 25; vibrate.start(); }
     })
-
     hmUI.createWidget(hmUI.widget.BUTTON, {
-      x: px(85),      
-      y: px(250) + px(480) * 3,
-      w: 150,
-      h: 150,
-      normal_src: 'food_3.png',
-      press_src: 'food_3_press.png',
-      click_func: () => {
-        console.log("food 3 BUTTON")
-
-        storages.food.append(`${time.utc};${6}`)
-
-        vibrate.stop()
-        vibrate.scene = 25
-        vibrate.start()  
-      }
+      x: 40, y: 235 + pageH * 3, w: 140, h: 140,
+      normal_src: 'food_3.png', press_src: 'food_3_press.png',
+      click_func: () => { storages.food.append(`${time.utc};${6}`); vibrate.stop(); vibrate.scene = 25; vibrate.start(); }
     })
-
     hmUI.createWidget(hmUI.widget.BUTTON, {
-      x: px(250),      
-      y: px(250) + px(480) * 3,
-      w: 150,
-      h: 150,
-      normal_src: 'food_4.png',
-      press_src: 'food_4_press.png',
-      click_func: () => {
-        console.log("food 4 BUTTON")
-
-        storages.food.append(`${time.utc};${9}`)             
-
-        vibrate.stop()
-        vibrate.scene = 25
-        vibrate.start()  
-      }
+      x: 210, y: 235 + pageH * 3, w: 140, h: 140,
+      normal_src: 'food_4.png', press_src: 'food_4_press.png',
+      click_func: () => { storages.food.append(`${time.utc};${9}`); vibrate.stop(); vibrate.scene = 25; vibrate.start(); }
     })
 
-        
-    // PAGE #4
+    // PAGE #4: ALCOHOL
     hmUI.createWidget(hmUI.widget.FILL_RECT, {
-      x: 0,
-      y: px(480) * 4,
-      w: px(480),
-      h: px(480),
-      color: 0xfcbf49
+      x: 0, y: pageH * 4, w: 390, h: pageH, color: 0xfcbf49
     })
-
     hmUI.createWidget(hmUI.widget.BUTTON, {
-      x: px(60),      
-      y: px(152) + px(480) * 4,
-      w: 175,
-      h: 175,
-      normal_src: 'alcohol_1.png',
-      press_src: 'alcohol_1_press.png',
-      click_func: () => {
-        console.log("alcohol 1 BUTTON")
-
-        storages.alcohol.append(`${time.utc};${1}`)  
-
-        vibrate.stop()
-        vibrate.scene = 25
-        vibrate.start()   
-      }
+      x: 40, y: 135 + pageH * 4, w: 140, h: 140,
+      normal_src: 'alcohol_1.png', press_src: 'alcohol_1_press.png',
+      click_func: () => { storages.alcohol.append(`${time.utc};${1}`); vibrate.stop(); vibrate.scene = 25; vibrate.start(); }
     })
-
     hmUI.createWidget(hmUI.widget.BUTTON, {
-      x: px(245),      
-      y: px(152) + px(480) * 4,
-      w: 175,
-      h: 175,
-      normal_src: 'alcohol_2.png',
-      press_src: 'alcohol_2_press.png',
-      click_func: () => {
-        console.log("alcohol 2 BUTTON")
-
-        storages.alcohol.append(`${time.utc};${3}`) 
-
-        vibrate.stop()
-        vibrate.scene = 25
-        vibrate.start() 
-      }
+      x: 210, y: 135 + pageH * 4, w: 140, h: 140,
+      normal_src: 'alcohol_2.png', press_src: 'alcohol_2_press.png',
+      click_func: () => { storages.alcohol.append(`${time.utc};${3}`); vibrate.stop(); vibrate.scene = 25; vibrate.start(); }
     })
 
-
-     // PAGE #5
+    // PAGE #5: PEE / POO
     hmUI.createWidget(hmUI.widget.FILL_RECT, {
-      x: 0,
-      y: px(480) * 5,
-      w: px(480),
-      h: px(480),
-      color: 0xeae2b7
+      x: 0, y: pageH * 5, w: 390, h: pageH, color: 0xeae2b7
     })
-
     hmUI.createWidget(hmUI.widget.BUTTON, {
-      x: px(60),      
-      y: px(152) + px(480) * 5,
-      w: 175,
-      h: 175,
-      normal_src: 'pee.png',
-      press_src: 'pee_press.png',
-      click_func: () => {
-        console.log("pee 1 BUTTON")
-
-        storages.pee.append(time.utc)         
-        
-        vibrate.stop()
-        vibrate.scene = 25
-        vibrate.start()   
-      }
+      x: 40, y: 135 + pageH * 5, w: 140, h: 140,
+      normal_src: 'pee.png', press_src: 'pee_press.png',
+      click_func: () => { storages.pee.append(time.utc); vibrate.stop(); vibrate.scene = 25; vibrate.start(); }
     })
-
     hmUI.createWidget(hmUI.widget.BUTTON, {
-      x: px(245),      
-      y: px(152) + px(480) * 5,
-      w: 175,
-      h: 175,
-      normal_src: 'poo.png',
-      press_src: 'poo_press.png',
-      click_func: () => {
-        console.log("poo 2 BUTTON")
-
-        storages.poo.append(time.utc)         
-      
-        vibrate.stop()
-        vibrate.scene = 25
-        vibrate.start()   
-      }
+      x: 210, y: 135 + pageH * 5, w: 140, h: 140,
+      normal_src: 'poo.png', press_src: 'poo_press.png',
+      click_func: () => { storages.poo.append(time.utc); vibrate.stop(); vibrate.scene = 25; vibrate.start(); }
     })
 
-
-    // PAGE #6
+    // PAGE #6: HEADACHE
     hmUI.createWidget(hmUI.widget.FILL_RECT, {
-      x: 0,
-      y: px(480) * 6,
-      w: px(480),
-      h: px(480),
-      color: 0xf9f7eb
+      x: 0, y: pageH * 6, w: 390, h: pageH, color: 0xf9f7eb
     })
-
-
     hmUI.createWidget(hmUI.widget.BUTTON, {
-      x: px(140),      
-      y: px(140) + px(480) * 6,
-      w: 200,
-      h: 200,
-      normal_src: 'headache.png',
-      press_src: 'headache_press.png',
-      click_func: () => {
-        console.log("headache BUTTON")
-
-        storages.headache.append(time.utc)           
-
-        vibrate.stop()
-        vibrate.scene = 25
-        vibrate.start()  
-      }
+      x: 95, y: 125 + pageH * 6, w: 200, h: 200,
+      normal_src: 'headache.png', press_src: 'headache_press.png',
+      click_func: () => { storages.headache.append(time.utc); vibrate.stop(); vibrate.scene = 25; vibrate.start(); }
     })
 
+    // Executa o envio sincronizado para o Supabase via app-side
+    fetchAndSendChunks(storages, messageBuilder);
   }
 });
